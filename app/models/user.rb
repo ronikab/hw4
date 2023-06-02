94c693c17cdb8721ff970fe6b4b68b5660e97183
Ronika Bhatti <ronika.bhatti@kellogg.northwestern.edu>
2023-06-02 19:10:09 +0000

    updates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    @user["username"] = params["user"]["username"]
+    @user["email"] = params["user"]["email"]
+    @user["password"] = BCrypt::Password.create(params["user"]["password"])
+    @user.save
+    redirect_to "/places"
+  end
+
 end
